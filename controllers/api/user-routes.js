ef56d19c5c546d762ef26fbe8b201acc8bbfa4a2
DJMIKE20 <m.zimm20@gmail.com>
2022-10-13 12:31:50 -0400

    added post route and tested in Insomnia, route and password hashing works

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,6 +35,24 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// POST, create a new user 
+router.post('/', (req,res) => {
+    // expecting { username: 'MZimm20', password: 'password' } // password is a min of 8 characters.
+    User.create({ 
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(createUserData => res.json(createUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+
+
 
 // exports routes 
 module.exports = router;
