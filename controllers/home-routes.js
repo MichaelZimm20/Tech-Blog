e88b41912e3067bdf68c9674c801a119b02ea5b3
DJMIKE20 <m.zimm20@gmail.com>
2022-10-16 17:17:38 -0400

    getting error on single-post, will not display data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,7 +42,47 @@ router.get('/', (req, res) => {
         });
 });
 
+// GET , create a single-post 
+router.get('/post/:id', (req,res) => {
+   
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'posted_note', 'created_at'],
 
+        // JOIN tables 
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(singlePostData => {
+            if (!singlePostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            // serialize data
+            const singlePost = singlePostData.get({ plain: true })
+            // render data to template, single-psot
+            res.render('single-post', { singlePost });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
+
+});
 
 // GET , get and render login handlebars page 
 router.get('/login', (req,res) => {
